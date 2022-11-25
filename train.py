a4f679c4a345b8a49923f10f36c3f25eca643696
Daniel Soares <danielsoares.data@outlook.com>
2022-11-25 16:52:27 -0300

    Improving the structure

--- train.py
+++ train.py
@@ -1,20 +1,20 @@
+from __future__ import annotations
+
+import joblib
+import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
 
-dados = pd.read_csv('Dados\dado_fake.csv')
+
+dados = pd.read_csv(r'Dados\dado_fake.csv')
 
 x = dados.iloc[:, :-1]
 y = dados.iloc[:, -1]
+x_treino, x_teste, y_treino, y_teste = train_test_split(
+    x, y, stratify=y, random_state=10,
+)
 
-x_treino, x_teste, y_treino, y_teste = train_test_split(x,
-                                                        y,
-                                                        stratify = y,
-                                                        random_state = 10)
-
-rf = RandomForestClassifier(random_state = 12)
+rf = RandomForestClassifier(random_state=12)
 
 rf.fit(x_treino, y_treino)
 
